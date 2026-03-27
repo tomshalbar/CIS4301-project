4edfbbe035ae5768e4790359ac5dd1ab0ae743ad
Tom <tomshalbar@gmail.com>
2026-03-27 12:16:22 -0400

    Finish set up

--- MARIADB_CREDS_EXAMPLE.py
+++ MARIADB_CREDS_EXAMPLE.py
@@ -1,3 +1,6 @@
+"""Don't touch this file. Create a new File called MARIADB_CRED.PY,
+then copy the dictionary below into it and fill in your info."""
+
 DB_CONFIG = {
     "username": "demo",
     "password": "",
